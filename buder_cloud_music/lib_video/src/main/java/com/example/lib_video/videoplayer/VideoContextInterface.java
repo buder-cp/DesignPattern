9eed64f176853fbb1b7592a592568c5743dafde5
buder <1254396779@qq.com>
2019-12-15 17:44:04 +0800

    1.视频播放组件的布局、逻辑开发 2.VideoSlot&CustomVideoView实现

---
+++ buder_cloud_music/lib_video/src/main/java/com/example/lib_video/videoplayer/VideoContextInterface.java
@@ -0,0 +1,13 @@
+package com.example.lib_video.videoplayer;
+
+/**
+ * 与应用层通信接口
+ */
+public interface VideoContextInterface {
+
+    void onVideoSuccess();
+
+    void onVideoFailed();
+
+    void onVideoComplete();
+}
---
+++ buder_cloud_music/lib_video/src/main/java/com/example/lib_video/videoplayer/core/VideoAdSlot.java
@@ -0,0 +1,161 @@
+package com.example.lib_video.videoplayer.core;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.example.lib_audio.app.AudioHelper;
+import com.example.lib_video.videoplayer.core.view.CustomVideoView;
+import com.example.lib_video.videoplayer.core.view.VideoFullDialog;
+import com.example.lib_video.videoplayer.utils.Utils;
+
+
+/**
+ * 视频业务逻辑层
+ */
+public class VideoAdSlot implements CustomVideoView.ADVideoPlayerListener {
+
+    private Context mContext;
+    /**
+     * UI
+     */
+    private CustomVideoView mVideoView;
+    private ViewGroup mParentView;
+    /**
+     * Data
+     */
+    private String mXAdInstance;
+    private SDKSlotListener mSlotListener;
+
+    public VideoAdSlot(String adInstance, SDKSlotListener slotLitener) {
+        mXAdInstance = adInstance;
+        mSlotListener = slotLitener;
+        mParentView = slotLitener.getAdParent();
+        mContext = mParentView.getContext();
+        initVideoView();
+    }
+
+    private void initVideoView() {
+        mVideoView = new CustomVideoView(mContext);
+        if (mXAdInstance != null) {
+            mVideoView.setDataSource(mXAdInstance);
+            mVideoView.setListener(this);
+        }
+        RelativeLayout paddingView = new RelativeLayout(mContext);
+        paddingView.setBackgroundColor(mContext.getResources().getColor(android.R.color.black));
+        paddingView.setLayoutParams(mVideoView.getLayoutParams());
+        mParentView.addView(paddingView);
+        mParentView.addView(mVideoView);
+    }
+
+    public void destroy() {
+        mVideoView.destroy();
+        mVideoView = null;
+        mContext = null;
+        mXAdInstance = null;
+    }
+
+    /**
+     * 实现play层接口
+     */
+    @Override
+    public void onClickFullScreenBtn() {
+        //获取videoview在当前界面的属性
+        Bundle bundle = Utils.getViewProperty(mParentView);
+        mParentView.removeView(mVideoView);
+        VideoFullDialog dialog =
+                new VideoFullDialog(mContext, mVideoView, mXAdInstance, mVideoView.getCurrentPosition());
+        dialog.setListener(new VideoFullDialog.FullToSmallListener() {
+            @Override
+            public void getCurrentPlayPosition(int position) {
+                backToSmallMode(position);
+            }
+
+            @Override
+            public void playComplete() {
+                bigPlayComplete();
+            }
+        });
+        dialog.setViewBundle(bundle); //为Dialog设置播放器数据Bundle对象
+        dialog.setSlotListener(mSlotListener);
+        dialog.show();
+        AudioHelper.pauseAudio();
+    }
+
+    private void backToSmallMode(int position) {
+        if (mVideoView.getParent() == null) {
+            mParentView.addView(mVideoView);
+        }
+        mVideoView.setTranslationY(0); //防止动画导致偏离父容器
+        mVideoView.isShowFullBtn(true);
+        mVideoView.mute(true);
+        mVideoView.setListener(this);
+        mVideoView.seekAndResume(position);
+        //恢复音乐
+        AudioHelper.resumeAudio();
+
+    }
+
+    private void bigPlayComplete() {
+        if (mVideoView.getParent() == null) {
+            mParentView.addView(mVideoView);
+        }
+        mVideoView.setTranslationY(0); //防止动画导致偏离父容器
+        mVideoView.isShowFullBtn(true);
+        mVideoView.mute(true);
+        mVideoView.setListener(this);
+        mVideoView.seekAndPause(0);
+    }
+
+    @Override
+    public void onClickVideo() {
+    }
+
+    @Override
+    public void onClickBackBtn() {
+    }
+
+    @Override
+    public void onClickPlay() {
+    }
+
+    @Override
+    public void onAdVideoLoadSuccess() {
+        if (mSlotListener != null) {
+            mSlotListener.onVideoLoadSuccess();
+        }
+    }
+
+    @Override
+    public void onAdVideoLoadFailed() {
+        if (mSlotListener != null) {
+            mSlotListener.onVideoFailed();
+        }
+    }
+
+    @Override
+    public void onAdVideoLoadComplete() {
+        if (mSlotListener != null) {
+            mSlotListener.onVideoComplete();
+        }
+        mVideoView.setIsRealPause(true);
+    }
+
+    @Override
+    public void onBufferUpdate(int time) {
+
+    }
+
+    //传递消息到appcontext层
+    public interface SDKSlotListener {
+
+        ViewGroup getAdParent();
+
+        void onVideoLoadSuccess();
+
+        void onVideoFailed();
+
+        void onVideoComplete();
+    }
+}
