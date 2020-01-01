626f178a37d20d8a502144a35d538d660c30c05d
buder <1254396779@qq.com>
2020-01-01 21:21:33 +0800

    门面模式到代理模式演进--原始架构

--- FacadeToAgentMode/app/src/main/java/com/example/facadetoagentmode/MainActivity.java
+++ FacadeToAgentMode/app/src/main/java/com/example/facadetoagentmode/MainActivity.java
@@ -2,12 +2,64 @@ package com.example.facadetoagentmode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private String URL = "http://c.3g.163.com/photo/api/set/0001%2F2250173.json";
+
+    RequestQueue mQueue;
+
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtn = findViewById(R.id.http_request);
+        mBtn.setOnClickListener(this);
+
+        mQueue = Volley.newRequestQueue(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.http_request) {
+            StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                    URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject obj = new JSONObject(response);
+                        String source = obj.getString("source");
+                        Toast.makeText(MainActivity.this, source, Toast.LENGTH_SHORT).show();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+
+            mQueue.add(stringRequest);
+        }
     }
 }
---
+++ FacadeToAgentMode/app/src/main/java/com/example/facadetoagentmode/PhotoSetInfo.java
@@ -0,0 +1,375 @@
+package com.example.facadetoagentmode;
+
+import java.util.List;
+
+public class PhotoSetInfo {
+    /**
+     * scover : http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLG00AP0001.jpg
+     * datatime : 2017-09-20 13:37:29
+     * createdate : 2017-04-16 14:23:34
+     * postid : PHOT24LDT000100A
+     * source : 四川日报
+     * relatedids : []
+     * photos : [{"timgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLD00AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250173.html#p=CI5B3HLD00AP0001","imgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/CI5B3HLD00AP0001.jpg","note":"4月16日上午，四川达州的天空黑压压的，早晨9点却光线昏暗。达州人的朋友圈被黑云刷爆。（来源：四川日报）","simgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLD00AP0001.jpg","newsurl":"#","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5B3HLD00AP0001.jpg","photoid":"CI5B3HLD00AP0001","imgtitle":"","squareimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5B3HLD00AP0001.jpg"},{"timgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLE00AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250173.html#p=CI5B3HLE00AP0001","imgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/CI5B3HLE00AP0001.jpg","note":"网友们纷纷调侃：这不是晚上么？有网友说：\u201c白天不懂夜的黑\u201d（来源：四川日报）","simgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLE00AP0001.jpg","newsurl":"#","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5B3HLE00AP0001.jpg","photoid":"CI5B3HLE00AP0001","imgtitle":"","squareimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5B3HLE00AP0001.jpg"},{"timgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLF00AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250173.html#p=CI5B3HLF00AP0001","imgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/CI5B3HLF00AP0001.jpg","note":"达州市气象台发布天气预报:16日有雷阵雨，雨量中到大雨，局地暴雨。（来源：四川日报）","simgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLF00AP0001.jpg","newsurl":"#","cimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/c_CI5B3HLF00AP0001.jpg","photoid":"CI5B3HLF00AP0001","imgtitle":"","squareimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/400x400_CI5B3HLF00AP0001.jpg"},{"timgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLG00AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250173.html#p=CI5B3HLG00AP0001","imgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/CI5B3HLG00AP0001.jpg","note":"4月16日上午，四川达州的天空黑压压的，早晨9点却光线昏暗。达州人的朋友圈被黑云刷爆。（来源：四川日报）","simgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLG00AP0001.jpg","newsurl":"#","cimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/c_CI5B3HLG00AP0001.jpg","photoid":"CI5B3HLG00AP0001","imgtitle":"","squareimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5B3HLG00AP0001.jpg"},{"timgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLH00AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250173.html#p=CI5B3HLH00AP0001","imgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/CI5B3HLH00AP0001.jpg","note":"网友们纷纷调侃：这不是晚上么？有网友说：\u201c白天不懂夜的黑\u201d（来源：四川日报）","simgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLH00AP0001.jpg","newsurl":"#","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5B3HLH00AP0001.jpg","photoid":"CI5B3HLH00AP0001","imgtitle":"","squareimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5B3HLH00AP0001.jpg"},{"timgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLI00AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250173.html#p=CI5B3HLI00AP0001","imgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/CI5B3HLI00AP0001.jpg","note":"达州市气象台发布天气预报:16日有雷阵雨，雨量中到大雨，局地暴雨。（来源：四川日报）","simgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLI00AP0001.jpg","newsurl":"#","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5B3HLI00AP0001.jpg","photoid":"CI5B3HLI00AP0001","imgtitle":"","squareimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/400x400_CI5B3HLI00AP0001.jpg"},{"timgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLJ00AP0001.jpg","photohtml":"http://news.163.com/photoview/00AP0001/2250173.html#p=CI5B3HLJ00AP0001","imgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/CI5B3HLJ00AP0001.jpg","note":"4月16日上午，四川达州的天空黑压压的，早晨9点却光线昏暗。达州人的朋友圈被黑云刷爆。（来源：四川日报）","simgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLJ00AP0001.jpg","newsurl":"#","cimgurl":"http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5B3HLJ00AP0001.jpg","photoid":"CI5B3HLJ00AP0001","imgtitle":"","squareimgurl":"http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5B3HLJ00AP0001.jpg"}]
+     * cover : http://img4.cache.netease.com/photo/0001/2017-04-16/CI5B3HLG00AP0001.jpg
+     * topicid : 00AP0001
+     * boardid : photoview_bbs
+     * tcover : http://img4.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLG00AP0001.jpg
+     * creator : 耿旭娜
+     * autoid :
+     * commenturl : http://comment.tie.163.com/PHOT24LDT000100A.html
+     * reporter :
+     * hideAd : false
+     * url : http://news.163.com/photoview/00AP0001/2250173.html
+     * setname : 四川达州雨前黑云压城 白昼如夜刷爆朋友圈
+     * imgsum : 7
+     * series :
+     * neteasecode :
+     * setid : 2250173
+     * clientadurl :
+     * settag : 四川，黑云，白昼
+     * channelid : 0001
+     * desc : 4月16日上午，四川达州的天空黑压压的，早晨9点却光线昏暗。达州人的朋友圈被黑云刷爆。
+     */
+
+    private String scover;
+    private String datatime;
+    private String createdate;
+    private String postid;
+    private String source;
+    private String cover;
+    private String topicid;
+    private String boardid;
+    private String tcover;
+    private String creator;
+    private String autoid;
+    private String commenturl;
+    private String reporter;
+    private boolean hideAd;
+    private String url;
+    private String setname;
+    private String imgsum;
+    private String series;
+    private String neteasecode;
+    private int setid;
+    private String clientadurl;
+    private String settag;
+    private String channelid;
+    private String desc;
+    private List<?> relatedids;
+    private List<PhotosBean> photos;
+
+    public String getScover() {
+        return scover;
+    }
+
+    public void setScover(String scover) {
+        this.scover = scover;
+    }
+
+    public String getDatatime() {
+        return datatime;
+    }
+
+    public void setDatatime(String datatime) {
+        this.datatime = datatime;
+    }
+
+    public String getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(String createdate) {
+        this.createdate = createdate;
+    }
+
+    public String getPostid() {
+        return postid;
+    }
+
+    public void setPostid(String postid) {
+        this.postid = postid;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getTopicid() {
+        return topicid;
+    }
+
+    public void setTopicid(String topicid) {
+        this.topicid = topicid;
+    }
+
+    public String getBoardid() {
+        return boardid;
+    }
+
+    public void setBoardid(String boardid) {
+        this.boardid = boardid;
+    }
+
+    public String getTcover() {
+        return tcover;
+    }
+
+    public void setTcover(String tcover) {
+        this.tcover = tcover;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getAutoid() {
+        return autoid;
+    }
+
+    public void setAutoid(String autoid) {
+        this.autoid = autoid;
+    }
+
+    public String getCommenturl() {
+        return commenturl;
+    }
+
+    public void setCommenturl(String commenturl) {
+        this.commenturl = commenturl;
+    }
+
+    public String getReporter() {
+        return reporter;
+    }
+
+    public void setReporter(String reporter) {
+        this.reporter = reporter;
+    }
+
+    public boolean isHideAd() {
+        return hideAd;
+    }
+
+    public void setHideAd(boolean hideAd) {
+        this.hideAd = hideAd;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSetname() {
+        return setname;
+    }
+
+    public void setSetname(String setname) {
+        this.setname = setname;
+    }
+
+    public String getImgsum() {
+        return imgsum;
+    }
+
+    public void setImgsum(String imgsum) {
+        this.imgsum = imgsum;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getNeteasecode() {
+        return neteasecode;
+    }
+
+    public void setNeteasecode(String neteasecode) {
+        this.neteasecode = neteasecode;
+    }
+
+    public int getSetid() {
+        return setid;
+    }
+
+    public void setSetid(int setid) {
+        this.setid = setid;
+    }
+
+    public String getClientadurl() {
+        return clientadurl;
+    }
+
+    public void setClientadurl(String clientadurl) {
+        this.clientadurl = clientadurl;
+    }
+
+    public String getSettag() {
+        return settag;
+    }
+
+    public void setSettag(String settag) {
+        this.settag = settag;
+    }
+
+    public String getChannelid() {
+        return channelid;
+    }
+
+    public void setChannelid(String channelid) {
+        this.channelid = channelid;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public List<?> getRelatedids() {
+        return relatedids;
+    }
+
+    public void setRelatedids(List<?> relatedids) {
+        this.relatedids = relatedids;
+    }
+
+    public List<PhotosBean> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<PhotosBean> photos) {
+        this.photos = photos;
+    }
+
+    public static class PhotosBean {
+        /**
+         * timgurl : http://img4.cache.netease.com/photo/0001/2017-04-16/t_CI5B3HLD00AP0001.jpg
+         * photohtml : http://news.163.com/photoview/00AP0001/2250173.html#p=CI5B3HLD00AP0001
+         * imgurl : http://img3.cache.netease.com/photo/0001/2017-04-16/CI5B3HLD00AP0001.jpg
+         * note : 4月16日上午，四川达州的天空黑压压的，早晨9点却光线昏暗。达州人的朋友圈被黑云刷爆。（来源：四川日报）
+         * simgurl : http://img3.cache.netease.com/photo/0001/2017-04-16/s_CI5B3HLD00AP0001.jpg
+         * newsurl : #
+         * cimgurl : http://img4.cache.netease.com/photo/0001/2017-04-16/c_CI5B3HLD00AP0001.jpg
+         * photoid : CI5B3HLD00AP0001
+         * imgtitle :
+         * squareimgurl : http://img3.cache.netease.com/photo/0001/2017-04-16/400x400_CI5B3HLD00AP0001.jpg
+         */
+
+        private String timgurl;
+        private String photohtml;
+        private String imgurl;
+        private String note;
+        private String simgurl;
+        private String newsurl;
+        private String cimgurl;
+        private String photoid;
+        private String imgtitle;
+        private String squareimgurl;
+
+        public String getTimgurl() {
+            return timgurl;
+        }
+
+        public void setTimgurl(String timgurl) {
+            this.timgurl = timgurl;
+        }
+
+        public String getPhotohtml() {
+            return photohtml;
+        }
+
+        public void setPhotohtml(String photohtml) {
+            this.photohtml = photohtml;
+        }
+
+        public String getImgurl() {
+            return imgurl;
+        }
+
+        public void setImgurl(String imgurl) {
+            this.imgurl = imgurl;
+        }
+
+        public String getNote() {
+            return note;
+        }
+
+        public void setNote(String note) {
+            this.note = note;
+        }
+
+        public String getSimgurl() {
+            return simgurl;
+        }
+
+        public void setSimgurl(String simgurl) {
+            this.simgurl = simgurl;
+        }
+
+        public String getNewsurl() {
+            return newsurl;
+        }
+
+        public void setNewsurl(String newsurl) {
+            this.newsurl = newsurl;
+        }
+
+        public String getCimgurl() {
+            return cimgurl;
+        }
+
+        public void setCimgurl(String cimgurl) {
+            this.cimgurl = cimgurl;
+        }
+
+        public String getPhotoid() {
+            return photoid;
+        }
+
+        public void setPhotoid(String photoid) {
+            this.photoid = photoid;
+        }
+
+        public String getImgtitle() {
+            return imgtitle;
+        }
+
+        public void setImgtitle(String imgtitle) {
+            this.imgtitle = imgtitle;
+        }
+
+        public String getSquareimgurl() {
+            return squareimgurl;
+        }
+
+        public void setSquareimgurl(String squareimgurl) {
+            this.squareimgurl = squareimgurl;
+        }
+    }
+}
