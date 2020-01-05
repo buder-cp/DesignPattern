65d3e9378e36d86924f3b19a06dad1a0c76397c1
buder <1254396779@qq.com>
2020-01-05 16:13:36 +0800

    组件化初探--module和application相互切换的gradle配置

---
+++ buder_DN_component/module2/src/main/java/com/example/module2/Module2Activity.java
@@ -0,0 +1,18 @@
+package com.example.module2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.dn_alan.module2.R;
+
+
+public class Module2Activity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module2);
+
+    }
+}
---
+++ buder_DN_component/module2/src/main/module/java/com/dn_alan/module2/Module2Application.java
@@ -0,0 +1,12 @@
+package com.dn_alan.module2;
+
+import android.app.Application;
+
+
+public class Module2Application extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
